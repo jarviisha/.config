dc7aa9f45cabac4ad5de6986f1d7707c372e762c
jarviisha <jarviisha@gmail.com>
2024-11-28 13:25:11 +0700

    feat: add new theme

--- nvim/lua/jarviisha/plugins/theme.lua
+++ nvim/lua/jarviisha/plugins/theme.lua
@@ -7,9 +7,28 @@ return {
 			vim.g.sonokai_enable_italic = "0"
 			vim.g.sonokai_float_style = "dim"
 			vim.g.sonokai_style = "andromeda"
-			vim.cmd.colorscheme("sonokai")
-			vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#354157", bg = "None" })
+			-- vim.cmd.colorscheme("sonokai")
+			-- vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#354157", bg = "None" })
 		end,
+	},
+	{
+		"olivercederborg/poimandres.nvim",
+		lazy = false,
+		priority = 1000,
+		-- config = function()
+		-- 	require("poimandres").setup({
+		-- 		bold_vert_split = false, -- use bold vertical separators
+		-- 		dim_nc_background = false, -- dim 'non-current' window backgrounds
+		-- 		disable_background = true, -- disable background
+		-- 		disable_float_background = false, -- disable background for floats
+		-- 		disable_italics = false, -- disable italics
+		-- 	})
+		-- end,
+
+		-- optionally set the colorscheme within lazy config
+		-- init = function()
+		-- 	vim.cmd("colorscheme poimandres")
+		-- end,
 	},
 	{
 		"craftzdog/solarized-osaka.nvim",
@@ -40,5 +59,20 @@ return {
 		-- 	})
 		-- 	vim.cmd("colorscheme cyberdream")
 		-- end,
+	},
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local nor = require("nordic")
+			nor.setup({
+				transparent = {
+					bg = true,
+					float = true,
+				},
+			})
+			nor.load()
+		end,
 	},
 }
