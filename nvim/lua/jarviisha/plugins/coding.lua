f85ca13175a1312d6925d8bb3460dd0a72a9f34f
jarviisha <jarviisha@gmail.com>
2024-12-07 17:40:16 +0700

    feat: add border to selected window

--- nvim/lua/jarviisha/plugins/coding.lua
+++ nvim/lua/jarviisha/plugins/coding.lua
@@ -7,7 +7,13 @@ return {
 				hint_enable = false, -- tắt gợi ý tự động
 				floating_window = true, -- bật cửa sổ nổi để hiển thị thông tin hàm
 				hi_parameter = "IncSearch", -- highlight tham số đã được truyền
+				handler_opts = {
+					border = "double",
+					-- border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+				},
 			})
+			-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1E1E2E" }) -- Nền của popup
+			vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#FFD700" }) -- Màu viền
 		end,
 	},
 }
